b0f474e726f0d8f3f3f7bb6564acb2ced5fc10b2
Johannes Vargas <johannes.vargas1@gmail.com>
2022-02-23 19:19:20 -0600

    commit tree ecbf4ca8c2f2c943c064b5aa9937c914179b234c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(outer = "*") {
+    return function (inner = "special") {
+        return `You are ${outer}${inner}${outer}!`
+    }
+}
+wrapAdjective()();
+    
+    
